785107a045d20865aac7295d77f149ce3b69c68c
Darren Petersen <darren.petersen@lullabot.com>
2014-08-13 11:38:32 -0400

    #9 - renamed functions in template.php to 'windup'...

--- template.php
+++ template.php
@@ -13,10 +13,10 @@
  *   An array of variables to pass to the theme template.
  * @param string $hook
  *   The name of the template being rendered. This is usually "html," but can
- *   also be "maintenance_page" since harbor_preprocess_maintenance_page()
+ *   also be "maintenance_page" since windup_preprocess_maintenance_page()
  *   calls this function to have consistent variables.
  */
-function harbor_preprocess_html(&$variables, $hook) {
+function windup_preprocess_html(&$variables, $hook) {
   // Attributes for html element.
   $variables['html_attributes_array'] = array(
     'lang' => $variables['language']->language,
@@ -45,14 +45,14 @@ function harbor_preprocess_html(&$variables, $hook) {
  * @param string $hook
  *   The name of the template being rendered. ("html" in this case.)
  */
-function harbor_process_html(&$variables, $hook) {
+function windup_process_html(&$variables, $hook) {
   $variables['html_attributes'] = drupal_attributes($variables['html_attributes_array']);
 }
 
 /**
  * Implements hook_preprocess_node().
  */
-function harbor_preprocess_node(&$variables) {
+function windup_preprocess_node(&$variables) {
   // Add an 'unpublished' variable.
   $variables['unpublished'] = (!$variables['status']) ? TRUE : FALSE;
 }
@@ -60,7 +60,7 @@ function harbor_preprocess_node(&$variables) {
 /**
  * Implements hook_page_alter().
  */
-function harbor_page_alter(&$page) {
+function windup_page_alter(&$page) {
   // Remove all the region wrappers.
   foreach (element_children($page) as $key => $region) {
     if (!empty($page[$region]['#theme_wrappers'])) {
