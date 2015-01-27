d52f50bb5ae714ec4a71498b5503b3b0d5bfaa44
Sally Young <sally@justafish.co.uk>
2015-01-27 16:58:09 +0000

    Update the default watch task's name to be consistent with the clean task

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      sass: {
+      css: {
         files:['scss/**/*.scss'],
         tasks: ['sass_globbing', 'sass'],
         options: {
