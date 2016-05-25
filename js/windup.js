ac532fa4e02e04983278c9cab2e937b0a12bb5a4
Wes Ruvalcaba <wesruv@gmail.com>
2016-05-24 20:41:18 -0400

    Fixing updates to eslint, eslint error in project  and npm build breakage
    Also added two fields to package.json to fix some dumb npm warnings
    Added npm scripts so dev doesn't need to install grunt globally

--- js/windup.js
+++ js/windup.js
@@ -1,13 +1,15 @@
 (function ($) {
 
+  'use strict';
+
   /**
    * Example function.
    * @see https://www.drupal.org/node/304258#drupal-behaviors
    */
   Drupal.behaviors.sayHello = {
-    attach: function(context) {
+    attach: function (context) {
       alert('Hello, world!');
     }
-  }
+  };
 
 })(jQuery);
