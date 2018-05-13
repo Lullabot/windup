29859853227fb58682f739f4a3aad1076820744e
Wes Ruvalcaba <wesruv@gmail.com>
2018-05-12 23:54:07 -0400

    Fixing multiple build issues
    Quoting attach property per eslint rules
    Adding grunt-cli locally so it isn't needed globally
    Rule in sass-lint needs a 's' to be valid
    Removing admin-tabs component because it gets us into trouble of deciding things like how sass vars and colors and all that should be declared in a project

--- js/windup.js
+++ js/windup.js
@@ -7,7 +7,7 @@
    * @see https://www.drupal.org/node/304258#drupal-behaviors
    */
   Drupal.behaviors.sayHello = {
-    attach: function (context) {
+    'attach': function (context) {
       // Do JavaScript things in here!
       // console.log('Hello, world!');
     }
